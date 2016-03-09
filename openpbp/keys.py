b046c45785eba38fdba50c2e161f194e0828be21
andrew-shaw <shawa1@tcd.ie>
2016-03-09 21:00:30 +0000

    keyring object

--- openpbp/keys.py
+++ openpbp/keys.py
@@ -7,22 +7,18 @@ these are:
 As these are common to all operations, we define them all here. Should they
 need to change, we can simply modify their definitions here.'''
 
-class PublicKey(object):
-    def __init__(self):
-        pass
 
+class Keyring(object):
+    def __init__(self, pubkeys):
+        '''
+        create a new (preliminary) keyring
 
-class PrivateKey(object):
-    def __init__(self):
-        pass
+        the keyring will not be usable until all of the signatures
+        have been collected and verified
 
-
-class GroupPSK(object):
-    def __init__(self):
-        pass
-
-
-class SessionKey(object):
-    def __init__(self):
-        pass
-
+        pubkeys: a list of public keys, in ascii armored format
+        '''
+        self.pubkeys = pubkeys
+        self.PSK = None
+        self.digest = None
+        self.signatures = []
