f8e3b0e709ede3da695b36c394736b08843155a7
andrew-shaw <shawa1@tcd.ie>
2016-03-13 20:19:39 +0000

    type hints to utils

--- openpbp/utils.py
+++ openpbp/utils.py
@@ -1,9 +1,9 @@
 from base64 import b64encode, b64decode
 
 
-def b64_string_to_bytes(string):
+def b64_string_to_bytes(string: str) -> bytes:
     return b64decode(string.encode('utf-8'))
 
 
-def bytes_to_b64_string(byte_string):
+def bytes_to_b64_string(byte_string: bytes) -> str:
     return b64encode(byte_string).decode('utf-8')
