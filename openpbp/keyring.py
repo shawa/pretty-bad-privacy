887f1b8f1ea6ce05271e57d572ba05a0024b33fc
andrew-shaw <shawa1@tcd.ie>
2016-03-14 18:52:29 +0000

    use aymmetric module over the stupidly chosen axolotyl

--- openpbp/keyring.py
+++ openpbp/keyring.py
@@ -1,34 +1,34 @@
-from typing import Tuple, List
-import axolotl_curve25519 as curve
+from typing import Tuple, List, Union
+import asymmetric
 import os
 
+
 from utils import b64_string_to_bytes, bytes_to_b64_string
-
-def generate_keypair() -> Tuple[bytes, bytes]:
-    '''generate a new keypair from the given curve'''
-    private_key = curve.generatePrivateKey(os.urandom(32))
-    pubkey = curve.generatePublicKey(private_key)
-    return private_key, pubkey
-
 
 class Keyring(object):
     def __init__(self, keys: List[bytes]) -> None:
         '''generate a keyring dict with the given keys'''
 
-        self.keys = [bytes_to_b64_string(k) for k in keys]
+        self.keys = keys
         self.sigs = []  # type: List[str]
 
+
     def _keystring(self) -> bytes:
-        '''return a bytes of the keyring values'''
-        return b''.join(b64_string_to_bytes(key)
-                        for key in self.keys)
+        '''return a bytes of the concatenated keyring PEM data values'''
+        return b''.join(self.keys)
 
 
-    def signature(self, private_key: bytes) -> str:
+    def signature(self, private_key: bytes, fmt=bytes) -> Union[str, bytes]:
         '''generate a signature for the given keyring'''
+        assert fmt in [str, bytes]
+
         nonce = os.urandom(64)
-        sig = curve.calculateSignature(nonce, private_key, self._keystring())
-        return bytes_to_b64_string(sig)
+        sig = asymmetric.sign(self._keystring(), private_key)
+
+        if fmt is str:
+            return bytes_to_b64_string(sig)
+        else:
+            return sig
 
 
     def _verify_sig(self, signature: str) -> bool:
@@ -36,13 +36,10 @@ class Keyring(object):
         verify that a signature is valid under one of the keys in the public
         key list
         '''
-
-        verifications = (curve.verifySignature(b64_string_to_bytes(pubkey),
-                                               self._keystring(),
-                                               b64_string_to_bytes(signature))
-                         for pubkey in self.keys)
-        valid_sigs = (verification is 0 for verification in verifications)
-        return any(valid_sigs)
+        sig = b64_string_to_bytes(signature) # type: bytes
+        concat = self._keystring()
+        return any(asymmetric.verify(concat, sig, pubkey)
+                   for pubkey in self.keys)
 
 
     def complete(self) -> bool:
