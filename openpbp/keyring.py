fb2b0ac17bdbee8bce287ae208c16780f94c6f33
andrew-shaw <shawa1@tcd.ie>
2016-03-14 11:14:53 +0000

    Tidy greatly

--- openpbp/keyring.py
+++ openpbp/keyring.py
@@ -7,8 +7,8 @@ from utils import b64_string_to_bytes, bytes_to_b64_string
 def generate_keypair() -> Tuple[bytes, bytes]:
     '''generate a new keypair from the given curve'''
     private_key = curve.generatePrivateKey(os.urandom(32))
-    public_key = curve.generatePublicKey(private_key)
-    return private_key, public_key
+    pubkey = curve.generatePublicKey(private_key)
+    return private_key, pubkey
 
 
 class Keyring(object):
@@ -16,7 +16,7 @@ class Keyring(object):
         '''generate a keyring dict with the given keys'''
 
         self.keys = [bytes_to_b64_string(k) for k in keys]
-        self.signatures = []  # type: List[str]
+        self.sigs = []  # type: List[str]
 
     def _keystring(self) -> bytes:
         '''return a bytes of the keyring values'''
@@ -31,17 +31,22 @@ class Keyring(object):
         return bytes_to_b64_string(sig)
 
 
-    def _verify_sig(self, public_key: str, signature: str):
+    def _verify_sig(self, signature: str) -> bool:
+        '''
+        verify that a signature is valid under one of the keys in the public
+        key list
+        '''
 
-        verification = curve.verifySignature(b64_string_to_bytes(public_key),
-                                             self._keystring(),
-                                             b64_string_to_bytes(signature))
-        return verification is 0
+        verifications = (curve.verifySignature(b64_string_to_bytes(pubkey),
+                                               self._keystring(),
+                                               b64_string_to_bytes(signature))
+                         for pubkey in self.keys)
+        valid_sigs = (verification is 0 for verification in verifications)
+        return any(valid_sigs)
 
 
     def complete(self) -> bool:
-        key_signatures = zip(self.keys, self.signatures)
-        verifications = (self._verify_sig(public_key, signature)
-                         for public_key, signature in key_signatures)
-
-        return all(verifications)
+        '''a complete keyring is a list of public keys, and a list of
+        signatures of that list of public keys, one per public key'''
+        assert(len(self.sigs) is len(self.keys))
+        return all(self._verify_sig(sig) for sig in self.sigs)
--- openpbp/test_keyring.py
+++ openpbp/test_keyring.py
@@ -20,7 +20,7 @@ class TestKeypair(unittest.TestCase):
 
 class TestKeyring(unittest.TestCase):
     def test_keyring_signature(self):
-        keypairs = [keyring.generate_keypair() for _ in range(3)]
+        keypairs = [keyring.generate_keypair() for _ in range(4)]
         priv_alice, pub_alice = keypairs[0]
         pub_alice = bytes_to_b64_string(pub_alice)
 
@@ -28,13 +28,13 @@ class TestKeyring(unittest.TestCase):
         ring = keyring.Keyring(pubkeys)
         sig = ring.signature(priv_alice)
 
-        self.assertTrue(ring._verify_sig(pub_alice, sig))
+        self.assertTrue(ring._verify_sig(sig))
 
     def test_keyring_integrity(self):
         keypairs = [keyring.generate_keypair() for _ in range(3)]
         ring = keyring.Keyring([kp[1] for kp in keypairs])
         sigs = [ring.signature(kp[0]) for kp in keypairs]
-        ring.signatures = sigs
+        ring.sigs = sigs
         self.assertTrue(ring.complete())
 
 if __name__ == '__main__':
