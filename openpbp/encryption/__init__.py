cd1fcbccb15966aec135d31a2d86368cf4cf48d7
andrew-shaw <shawa1@tcd.ie>
2016-03-20 20:57:32 +0000

    packing tests, add serializatino

--- openpbp/encryption/__init__.py
+++ openpbp/encryption/__init__.py
@@ -1,20 +1,38 @@
-from typing import List, Tuple
+from typing import List, Tuple, Any
 from struct import pack, unpack
 from cryptography.fernet import Fernet
 from keyring import Keyring
 
+from utils import b64_string_to_bytes, bytes_to_b64_string
+
 import asymmetric
 
+MAGIC_MARKER = '|#|buckfast|@|'
 
 def pack_group_block(message: bytes, keys: List[bytes]) -> Tuple[str, bytes]:
-    fmt = ('c' * len(message) + ''.join('c' * len(key) for key in keys))
+    fmt = ('{}s'.format(len(message)) +
+           ''.join(['{}s'.format(len(k)) for k in keys]))
     block = pack(fmt, message, *keys)
     return fmt, block
 
 
-def unpack_group_block(fmt: str, block: bytes) -> Tuple[bytes, List[bytes]]:
-    message, keys = struct.unpack(fmt, block)
-    return message, keys
+def unpack_group_block(fmt: str, block: bytes) -> Tuple[Any, List[Any]]:
+    message, *keys = unpack(fmt, block)
+    return message, list(keys)
+
+
+def serialize_group_block(fmt: str, block: bytes) -> str:
+    block_serial = bytes_to_b64_string(block)
+    return ('{fmt}{marker}{block}'
+            .format(fmt=fmt, marker=MAGIC_MARKER, block=block))
+
+def deserialize_group_block(serialized: str) -> Tuple[str, bytes]:
+    if MAGIC_MARKER not in serialized:
+        raise ValueError('bad serialized block given')
+
+    fmt, block_serial = serialized.split(MAGIC_MARKER)
+    block = b64_string_to_bytes(block_serial)
+    return fmt, block
 
 
 def encrypt(ring: Keyring, privkey: bytes,
@@ -39,7 +57,7 @@ def encrypt(ring: Keyring, privkey: bytes,
     # TODO:  She places KB and Cs together, to form the Group Ciphertext Cg.
     # Cg, and thus the original file P may now be only be decrypted by a member
     # of the group.
-    group_block = message + ''b''.join(k for k in keys)
+    fmt, group_block = pack_group_block(message, keys)
 
     # TODO:  Finally, she signs Gg with her private key, producing Sg so that
     # each member may verify the file’s integrity, and that the sender is
