28dc6bb45b5c7891cc7b042d652153415902a557
andrew-shaw <shawa1@tcd.ie>
2016-03-30 17:50:24 +0100

    failing thing

--- openpbp/encryption/__init__.py
+++ openpbp/encryption/__init__.py
@@ -25,14 +25,13 @@ def pack_keys_and_ciphertext(keys: List[bytes],
     fmt_k = ''.join(['{}s'.format(len(key)) for key in keys])
     fmt_b = '{}s'.format(len(ciphertext))
     fmt = fmt_k + fmt_b
-    packed = pack(fmt, keys, ciphertext)
+    packed = pack(fmt, *(tuple(keys) + (ciphertext, )))
     return fmt, packed
 
 
 def unpack_keys_and_ciphertext(fmt, packed):
     vals = unpack(fmt, packed)
-    keys = vals[:-1]
-    ciphertext = vals[-1]
+    *keys, ciphertext = vals
     return keys, ciphertext
 
 
@@ -43,22 +42,33 @@ def pack_sig_and_block(block_fmt: str,
     fmt_sig = '{}s'.format(len(sig))
     fmt_cipher_block = '{}s'.format(len(ciphertext_block))
     fmt = fmt_block_fmt + fmt_sig + fmt_cipher_block
-    packed = pack(fmt, block_fmt, sig, ciphertext_block)
+    packed = pack(fmt, block_fmt.encode('utf-8'), sig, ciphertext_block)
     return fmt, packed
 
 
-def unpack_sig_and_block(fmt, packed):
+def unpack_sig_and_block(fmt: str, packed: bytes):
     block_fmt, sig, ciphertext_block = unpack(fmt, packed)
-    return block_fmt, sig, ciphertext_block
+    return block_fmt.decode('utf-8'), sig, ciphertext_block
+
+
+_SEPARATOR = '|'
+def serialize_everything(fmt: str, everything_packed: bytes) -> str:
+    serialized = bytes_to_b64_string(everything_packed)
+    return '{}{}{}'.format(fmt, _SEPARATOR, serialized)
+
+
+def deserialize_everything(serialized_everything_packed: str):
+    fmt, everything_packed = serialized_everything_packed.split(_SEPARATOR)
+    deserialized = b64_string_to_bytes(everything_packed)
+    return fmt, everything_packed
 
 
 def encrypt(plaintext: bytes, ring: Keyring, privkey: bytes) -> str:
-    symmetric_ciphertext, symmetric_key = _symmetric_encrypt(plaintext)
-    group_keys = ring.encrypt(symmetric_key)
-
-    fmt, key_ciphertext_block = pack_keys_and_ciphertext(group_keys,
-                                                         symmetric_ciphertext)
+    symm_ciphertext, symm_key = _symmetric_encrypt(plaintext)
+    group_keys = ring.encrypt(symm_key)
+    fmt, ciphertext_block = pack_keys_and_ciphertext(group_keys, symm_ciphertext)
     sig = asymmetric.sign(key_ciphertext_block, privkey)
+    fmt, packed = pack_sig_and_block(fmt, sig, ciphertext_block)
 
 
 def decrypt(message: str, pubkey: bytes, privkey: bytes) -> bytes:
--- openpbp/test/test_encryption.py
+++ openpbp/test/test_encryption.py
@@ -8,7 +8,7 @@ import keyring
 from utils import b64_string_to_bytes
 
 from hypothesis import given
-from hypothesis.strategies import binary, lists
+from hypothesis.strategies import binary, text, lists
 
 class Test__symmetric_encrypt_decrypt(unittest.TestCase):
     @given(binary())
@@ -18,6 +18,35 @@ class Test__symmetric_encrypt_decrypt(unittest.TestCase):
         self.assertIsNotNone(key)
         got_plaintext = encryption._symmetric_decrypt(ciphertext, key)
         self.assertEqual(got_plaintext, plaintext)
+
+
+class Test_pack_keys_and_ciphertext(unittest.TestCase):
+    @given(binary(), binary(), binary(), binary())
+    def test_pack_unpack_invert(self, a, b, c, ciphertext):
+        keys = [a, b, c]
+        fmt, packed = encryption.pack_keys_and_ciphertext(keys, ciphertext)
+        self.assertIsNotNone(fmt)
+        self.assertIsNotNone(packed)
+        got_keys, got_ciphertext = encryption.unpack_keys_and_ciphertext(fmt, packed)
+        self.assertEqual(keys, got_keys)
+        self.assertEqual(ciphertext, got_ciphertext)
+
+
+class Test_pack_sig_and_block(unittest.TestCase):
+    @given(text(min_size=1), binary(min_size=1), binary(min_size=1))
+    def test_pack_sig_and_block_invert(self, block_fmt, sig, ciphertext_block):
+        fmt, packed = encryption.pack_sig_and_block(block_fmt, sig, ciphertext_block)
+        self.assertIsNotNone(fmt)
+        self.assertIsNotNone(packed)
+
+        got_block_fmt, got_sig, got_ciphertext_block = encryption.unpack_sig_and_block(fmt, packed)
+        self.assertEqual(block_fmt, got_block_fmt)
+        self.assertEqual(sig, got_sig)
+        self.assertEqual(ciphertext_block, got_ciphertext_block)
+
+
+class Test_serialize_everything(unittest.TestCase):
+    pass
 
 class Test_encrypt(unittest.TestCase):
     def setUp(self):
@@ -35,6 +64,3 @@ class Test_encrypt(unittest.TestCase):
 
         ring.sigs = sigs
         self.assertTrue(ring.complete)
-
-    def test_encrypt(self):
-        self.assertTrue(True)
